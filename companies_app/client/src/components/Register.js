3fc5ea8a80015180710e30909bd16c9ff66d2d76
animchenko <anastasiya.nimchenko@gmail.com>
2019-06-16 23:37:38 +0300

    add companies_app

---
+++ companies_app/client/src/components/Register.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import { register } from './UserFunctions'
+
+class Register extends Component {
+    constructor() {
+        super()
+        this.state = {
+            first_name: '',
+            last_name: '',
+            phone_number: '',
+            nick_name: '',
+            description: '',
+            email: '',
+            password: '',
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange (e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit (e) {
+        e.preventDefault()
+
+        const user = {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            phone_number: this.state.phone_number,
+            nick_name: this.state.nick_name,
+            description: this.state.description,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        register(user).then(res => {
+            this.props.history.push(`/login`)
+        })
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mt-5 mx-auto">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                            <div className="form-group">
+                                <label htmlFor="first_name">First Name</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="first_name"
+                                    placeholder="Enter First Name"
+                                    value={this.state.first_name}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="last_name">Last Name</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="last_name"
+                                    placeholder="Enter Last Name"
+                                    value={this.state.last_name}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phone_number">Phone number</label>
+                                <input type="phone_number"
+                                    className="form-control"
+                                    name="phone_number"
+                                    placeholder="Enter phone number"
+                                    value={this.state.phone_number}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="nick_name">Nick name</label>
+                                <input type="nick_name"
+                                    className="form-control"
+                                    name="nick_name"
+                                    placeholder="Enter nick name"
+                                    value={this.state.nick_name}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="description">Description</label>
+                                <input type="description"
+                                    className="form-control"
+                                    name="description"
+                                    placeholder="Enter description"
+                                    value={this.state.description}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Enter Email"
+                                    value={this.state.email}
+                                    onChange={this.onChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    name="password"
+                                    placeholder="Enter Password"
+                                    value={this.state.password}
+                                    onChange={this.onChange} />
+                            </div>
+                            <button type="submit" className="btn btn-lg btn-primary btn-block">
+                                Register
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Register
